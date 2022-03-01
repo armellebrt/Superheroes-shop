2f124a6ab6f4132833a9126651ee0a75294d6d9e
Armelle <armelle.bertin07@gmail.com>
2022-03-01 15:50:03 +0100

    first commit

--- src/Form/ProductsType.php
+++ src/Form/ProductsType.php
@@ -4,20 +4,63 @@ namespace App\Form;
 
 use App\Entity\Products;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
 
 class ProductsType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('sku')
-            ->add('name')
-            ->add('price')
-            ->add('image')
-            ->add('stock')
+            ->add('sku', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'label' => "SKU"
+            ])
+            ->add('name', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'label' => "Nom"
+            ])
+            ->add('price', NumberType::class, [
+                    'attr' => [
+                        'class' => 'form-control'
+                    ],
+                'label' => "Prix"
+                ])
+            ->add('image', TextareaType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('stock', NumberType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
         ;
+
+//        ->add('password', RepeatedType::class, [
+//        'type' => PasswordType::class,
+//        'first_options'  => ['label' => 'Mot de passe',
+//            'attr' => [
+//                'class' => 'form-control trapezoid'
+//            ],
+//        ],
+//        'second_options' => ['label' => 'Confirmation du mot de passe',
+//            'attr' => [
+//                'class' => 'form-control trapezoid'
+//            ],
+//        ],
+//        'invalid_message' => 'form.user.register.errors.passwords_mismatch'
+//    ]);
+
     }
 
     public function configureOptions(OptionsResolver $resolver): void
--- src/Form/UsersType.php
+++ src/Form/UsersType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,18 +16,26 @@ class UsersType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('pseudo')
-            ->add('email', EmailType::class)
+            ->add('pseudo', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
             ->add('password', RepeatedType::class, [
                 'type' => PasswordType::class,
                 'first_options'  => ['label' => 'Mot de passe',
                     'attr' => [
-                    'class' => 'form-control trapezoid'
+                    'class' => 'form-control '
                 ],
                     ],
                 'second_options' => ['label' => 'Confirmation du mot de passe',
                     'attr' => [
-                        'class' => 'form-control trapezoid'
+                        'class' => 'form-control '
                     ],
                 ],
                 'invalid_message' => 'form.user.register.errors.passwords_mismatch'
